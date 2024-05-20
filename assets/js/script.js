42a7b86cc45df4716c7cd0dd3ce197afb8cd8edc
Hooman-evolve <mr.mervett@gmail.com>
2024-05-20 18:13:15 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+// script.js
+document.addEventListener('scroll', function() {
+  const nav = document.querySelector('.sci-fi-nav');
+  if (window.scrollY > 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
 
